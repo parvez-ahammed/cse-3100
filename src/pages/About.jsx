bea1f27ca7098f0814b349fc8d779979f398e0e0
Sakibul Hassan Shovon <shovon.cse50@gmail.com>
2025-06-24 02:31:59 +0600

    test

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ export default function About() {
   return (
     <div className="container my-4">
       <h2>About Rick & Morty Explorer</h2>
+      <h3>Explore the Multiverses of Rick & Morty</h3>
       <p>
         This is a simple React app that lets you browse characters from the Rick
         & Morty universe. Built with students of CSE-3100.
