f27f2ee3350070e30f7a24fa7901f09a3438331d
Pabak Dev <53967227+pabak-dev@users.noreply.github.com>
2025-06-24 23:53:01 +0600

    add: new design for character card & details

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,18 +1,24 @@
 export default function CharacterCard({ character }) {
   return (
-    <div className="card">
-      <img
-        src={character.image}
-        className="card-img-top"
-        alt={character.name}
-      />
-      <div className="card-body">
-        <h5 className="card-title">{character.name}</h5>
-        <p className="card-text">
-          <strong>Status:</strong> {character.status} <br />
-          <strong>Species:</strong> {character.species}
+    <div className="bg-gray-50 rounded-3xl overflow-hidden shadow-sm hover:shadow-xl transition-shadow duration-300">
+      <img src={character.image} className="w-full h-64 object-cover" />
+      <div className="p-4">
+        <h5 className="text-lg text-center font-bold text-black mb-3">
+          {character.name}
+        </h5>
+        <p className="text-gray-700">
+          <span className="font-semibold">Status:</span> {character.status}
+          <br />
+          <span className="font-semibold">Species:</span> {character.species}
         </p>
-        <button>View Details</button>
+        <button
+          className="mt-4 w-full rounded-lg bg-gray-900 text-white py-2 font-medium hover:bg-gray-800 transition"
+          onClick={() => {
+            window.location.href = `/character/${character.id}`;
+          }}
+        >
+          Details
+        </button>
       </div>
     </div>
   );
--- src/pages/CharacterDetail.jsx
+++ src/pages/CharacterDetail.jsx
@@ -11,16 +11,62 @@ export default function CharacterDetail() {
       .then(setCharacter);
   }, [id]);
 
-  if (!character) return <p>Loading...</p>;
-
-  return (
-    <div className="container my-4">
-      <h2>{character.name}</h2>
-      <img src={character.image} alt={character.name} className="img-fluid" />
-      <p>
-        <strong>Status:</strong> {character.status} <br />
-        <strong>Species:</strong> {character.species} <br />
-      </p>
-    </div>
-  );
+  if (!character)
+    return <p className="p-4 text-gray-500 text-center">Please wait..</p>;
+  else
+    return (
+      <div className="max-w-6xl mx-auto p-4 mt-4 flex flex-col md:flex-row rounded-lg">
+        <div className="flex flex-col items-center md:items-start">
+          <img
+            src={character.image}
+            className="rounded-lg w-100 h-100 object-cover"
+          />
+        </div>
+        <div className="mt-6 md:ml-8 md:mt-0 flex-1 space-y-3 text-gray-600">
+          <h1 className="text-5xl font-extrabold text-black my-8">
+            {character.name}
+          </h1>
+          <div className="flex justify-between border-b border-gray-300 pb-2">
+            <span className="font-bold text-gray-800 text-2xl lg:text-3xl">
+              Status
+            </span>
+            <span className="font-semibold text-2xl lg:text-3xl">
+              {character.status}
+            </span>
+          </div>
+          <div className="flex justify-between border-b border-gray-300 pb-2">
+            <span className="font-bold text-gray-800 text-2xl lg:text-3xl">
+              Species
+            </span>
+            <span className="font-semibold text-2xl lg:text-3xl">
+              {character.species}
+            </span>
+          </div>
+          <div className="flex justify-between border-b border-gray-300 pb-2">
+            <span className="font-bold text-gray-800 text-2xl lg:text-3xl">
+              Origin
+            </span>
+            <span className="font-semibold text-2xl lg:text-3xl">
+              {character.origin?.name}
+            </span>
+          </div>
+          <div className="flex justify-between border-b border-gray-300 pb-2">
+            <span className="font-bold text-gray-800 text-2xl lg:text-3xl">
+              Location
+            </span>
+            <span className="font-semibold text-2xl lg:text-3xl">
+              {character.location?.name}
+            </span>
+          </div>
+          <div className="flex justify-between">
+            <span className="font-bold text-gray-900 text-2xl lg:text-3xl">
+              Episodes
+            </span>
+            <span className="font-semibold text-2xl lg:text-3xl">
+              {character.episode?.length}
+            </span>
+          </div>
+        </div>
+      </div>
+    );
 }
