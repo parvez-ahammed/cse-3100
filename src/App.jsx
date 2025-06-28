380c694675a7ff14b0b2774fcd165a284021c199
shahadat200 <shahadat.aust2@gmail.com>
2025-06-29 00:53:56 +0600

    update for navigate view details

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import  CharacterDetail from "./pages/CharacterDetail";
 
 export default function App() {
   return (
     <BrowserRouter>
+        
+      
       <Routes>
+       
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/character/:id" element={<CharacterDetail />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
