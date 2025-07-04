b71a88c35da9326bc66a5b084a62e38e94ca0ec7
mahir2995 <139786025+amaterasu0001@users.noreply.github.com>
2025-07-04 16:01:04 +0600

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import CharacterDetail from "./pages/CharacterDetail"; // This is the character detail page
+import CharacterDetail from "./pages/CharacterDetail";
+import Navbar from "./components/Navbar"; // Import the Navbar component
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar /> {/* Render the Navbar here, outside of Routes so it's on all pages */}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='/character/:id' element={<CharacterDetail />} /> {/* This must be added */}
+        <Route path='/character/:id' element={<CharacterDetail />} />
       </Routes>
     </BrowserRouter>
   );
