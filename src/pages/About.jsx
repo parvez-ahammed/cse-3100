594557587392cd019fd720bb1a9f2533f179f825
Salman Faresi <faresisalman999@gmail.com>
2025-07-05 19:27:16 +0600

    Adding About us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,26 @@
+// About page component - displays static information about the app, developer, and a quote
 export default function About() {
   return (
     <div className="container my-4">
+      {/* App description */}
       <h2>About Rick & Morty Explorer</h2>
       <p>
-        This is a simple React app that lets you browse characters from the Rick
-        & Morty universe. Built with students of CSE-3100.
+        <strong>Rick & Morty Explorer</strong> is a React single-page application that allows users to browse, search, and filter characters from the Rick and Morty universe. You can view detailed information about each character, use advanced filters, and enjoy a modern, responsive UI. This app is built as part of the CSE-3100 Software Development Lab assignment.
       </p>
+      <hr />
+      {/* Developer info */}
+      <h4>Developer</h4>
+      <p>
+        <strong> Salman Faresi</strong><br />
+        Student, CSE-3100 Software Development Lab
+      </p>
+      <hr />
+      {/* Favorite quote section */}
+      <h4>Favorite Rick & Morty Quote</h4>
+      <blockquote className="blockquote">
+        <p>"Wubba Lubba Dub Dub!"</p>
+        <footer className="blockquote-footer">Rick Sanchez</footer>
+      </blockquote>
     </div>
   );
 }
