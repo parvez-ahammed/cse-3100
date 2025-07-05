4c47bc09da4012bf130470d64b082218c55ba5b9
Bijoy <bijoy.ahmed12555@gmail.com>
2025-07-05 17:13:31 +0600

    add: routing and navbar setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import CharacterDetail from "./pages/CharacterDetail";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <BrowserRouter>
+          <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/character/:id" element={<CharacterDetail />} />
+
       </Routes>
     </BrowserRouter>
   );
