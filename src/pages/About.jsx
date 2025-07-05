61714b0dc7f81278b1c00f07eeac7a126b3afa1f
Bijoy <bijoy.ahmed12555@gmail.com>
2025-07-05 16:58:31 +0600

    Add: improved about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,10 +2,17 @@ export default function About() {
   return (
     <div className="container my-4">
       <h2>About Rick & Morty Explorer</h2>
+      <h4>About this app</h4>
       <p>
-        This is a simple React app that lets you browse characters from the Rick
-        & Morty universe. Built with students of CSE-3100.
+        Rick & Morty Explorer is a simple React app that lets you browse and search for characters from the Rick & Morty universe. You can view details, filter by status, and enjoy a modern UI with dark mode support.
       </p>
+      <h4>Developer</h4>
+      <p>
+          I'm <span style={{ color: "#0984e3" }}>Tajuddin Ahmed</span>, the developer of this web app. Built with React, it is designed to help users discover and explore Rick and Morty, the American animated science-fiction sitcom.
+      </p>
+      <h4>Favorite Rick & Morty Quote</h4>
+      <blockquote style={{ fontStyle: "italic", borderLeft: "4px solid #0984e3", paddingLeft: "1rem", color: "#636e72" }}>
+I haven’t watched Rick and Morty, so I don’t have a personal quote and prefer not to use one from the internet or AI.      </blockquote>
     </div>
   );
 }
