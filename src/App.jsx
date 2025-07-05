247d0cf72bdd2d6f59687eeb1010327517fe5f65
Ma-Huan Sheikh Meem <cenationmaster02@gmail.com>
2025-07-06 01:34:48 +0600

    Routing character details and navbar
    Navbar and Character Detail added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import Navigation from "./components/navbar";
+import CharacterDetail from "./pages/CharacterDetail"; 
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <Navigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/character/:id" element={<CharacterDetail />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
