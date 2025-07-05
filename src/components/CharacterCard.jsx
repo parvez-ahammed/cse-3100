032107773968053f50662ba6315307ac7670e884
Bijoy <bijoy.ahmed12555@gmail.com>
2025-07-05 17:15:26 +0600

    Add:Character details

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,18 +1,15 @@
+import { Link } from "react-router-dom";
+
 export default function CharacterCard({ character }) {
   return (
-    <div className="card">
-      <img
-        src={character.image}
-        className="card-img-top"
-        alt={character.name}
-      />
+    <div className="card h-100">
+      <img src={character.image} className="card-img-top" alt={character.name} />
       <div className="card-body">
         <h5 className="card-title">{character.name}</h5>
-        <p className="card-text">
-          <strong>Status:</strong> {character.status} <br />
-          <strong>Species:</strong> {character.species}
-        </p>
-        <button>View Details</button>
+        <p className="card-text">{character.status} - {character.species}</p>
+        <Link to={`/character/${character.id}`} className="btn btn-primary" >
+          Details
+        </Link>
       </div>
     </div>
   );
