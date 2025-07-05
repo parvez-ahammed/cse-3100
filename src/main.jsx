00803ca26ef99fb8a12e25e3ff3fafadc2c09bca
Salman Faresi <faresisalman999@gmail.com>
2025-07-05 19:39:45 +0600

    Updating the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
+// Entry point for the React application
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// Import global styles
 import "./index.css";
+// Import the root App component
 import App from "./App.jsx";
 
+// Create a React root and render the App wrapped in StrictMode
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
