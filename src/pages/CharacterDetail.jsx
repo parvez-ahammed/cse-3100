623cb42c28a8000a4b5f80e4204e85e0b587ccc8
Prionty Saha <priontysaha003@gmail.com>
2025-07-11 15:57:37 +0600

    add : Design for Character detail and card

--- src/pages/CharacterDetail.jsx
+++ src/pages/CharacterDetail.jsx
@@ -11,16 +11,44 @@ export default function CharacterDetail() {
       .then(setCharacter);
   }, [id]);
 
-  if (!character) return <p>Loading...</p>;
+  if (!character)
+    return <div className="text-center my-10 text-lg">Loading...</div>;
 
   return (
-    <div className="container my-4">
-      <h2>{character.name}</h2>
-      <img src={character.image} alt={character.name} className="img-fluid" />
-      <p>
-        <strong>Status:</strong> {character.status} <br />
-        <strong>Species:</strong> {character.species} <br />
-      </p>
+    <div className="flex justify-center my-10">
+      <div className="bg-white rounded-2xl shadow-lg w-full max-w-md">
+        <img
+          src={character.image}
+          alt={character.name}
+          className="w-full h-72 object-cover rounded-t-2xl"
+        />
+        <div className="p-6">
+          <h2 className="text-2xl font-bold text-center mb-4">
+            {character.name}
+          </h2>
+          <div className="space-y-2">
+            <p>
+              <span className="font-semibold">Status:</span> {character.status}
+            </p>
+            <p>
+              <span className="font-semibold">Species:</span>{" "}
+              {character.species}
+            </p>
+            <p>
+              <span className="font-semibold">Origin:</span>{" "}
+              {character.origin?.name}
+            </p>
+            <p>
+              <span className="font-semibold">Last known location:</span>{" "}
+              {character.location?.name}
+            </p>
+            <p>
+              <span className="font-semibold">Number of episodes:</span>{" "}
+              {character.episode.length}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
