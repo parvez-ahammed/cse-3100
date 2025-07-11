fe81b6c2275ef39e00f7d62578092bd284eef18f
Nafi Bin Zafar <nafi.zafar179@gmail.com>
2025-07-11 16:28:44 +0600

    add: Root Layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { GiStairs } from "react-icons/gi";
+import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav className="flex justify-around items-center p-7 bg-gray-900 text-white">
@@ -7,24 +8,30 @@ export default function Navbar() {
         <h1 className="font-extrabold">Rick & Morty Explorer</h1>
       </div>
       <ul className="flex gap-7">
-        <li className="relative group">
-          <span className="cursor-pointer hover:text-purple-300 transition-all duration-300">
-            Home
-          </span>
-          <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-purple-400 transition-all duration-300 group-hover:w-full"></span>
-        </li>
-        <li className="relative group">
-          <span className="cursor-pointer hover:text-cyan-300 transition-all duration-300">
-            About
-          </span>
-          <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-cyan-400 transition-all duration-300 group-hover:w-full"></span>
-        </li>
-        <li className="relative group">
-          <span className="cursor-pointer hover:text-green-300 transition-all duration-300">
-            Contact
-          </span>
-          <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-green-400 transition-all duration-300 group-hover:w-full"></span>
-        </li>
+        <Link to={"/"}>
+          <li className="relative group">
+            <span className="cursor-pointer hover:text-purple-300 transition-all duration-300">
+              Home
+            </span>
+            <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-purple-400 transition-all duration-300 group-hover:w-full"></span>
+          </li>
+        </Link>
+        <Link to={"/about"}>
+          <li className="relative group">
+            <span className="cursor-pointer hover:text-cyan-300 transition-all duration-300">
+              About
+            </span>
+            <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-cyan-400 transition-all duration-300 group-hover:w-full"></span>
+          </li>
+        </Link>
+        <Link to={"/contact"}>
+          <li className="relative group">
+            <span className="cursor-pointer hover:text-green-300 transition-all duration-300">
+              Contact
+            </span>
+            <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-green-400 transition-all duration-300 group-hover:w-full"></span>
+          </li>
+        </Link>
       </ul>
     </nav>
   );
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,18 +2,25 @@ import { createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import RootLayout from "./RootLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
   },
 ]);
