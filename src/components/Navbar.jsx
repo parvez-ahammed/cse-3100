a5463898c6fb514e23f4edd7c160abf4806e0180
mahdisByte <mahdikhan.chowdhury@gmail.com>
2025-07-12 00:05:26 -0700

    Navbar color and style updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,27 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
-      <div className="container">
-        <Link className="navbar-brand" to="/">Rick & Morty Explorer</Link>
+    <nav
+      className="navbar navbar-expand-lg mb-4"
+      style={{
+        background: "linear-gradient(to right, #0f0c29, #302b63, #24243e)",
+        boxShadow: "0 0 20px #00ffe0",
+        borderBottom: "2px solid #00ffe0",
+      }}
+    >
+      <div className="container" style={{ maxWidth: "960px" }}>
+        <Link
+          className="navbar-brand"
+          to="/"
+          style={{
+            color: "#00ffe0",
+            fontSize: "2rem",
+            fontWeight: "700",
+            textShadow: "0 0 10px #00ffe0",
+          }}
+        >
+          Rick & Morty Explorer
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -17,13 +35,46 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+              <Link
+                className="nav-link"
+                to="/"
+                style={{
+                  color: "#00ffe0",
+                  fontSize: "1.25rem",
+                  fontWeight: "600",
+                  textShadow: "0 0 5px #00ffe0",
+                }}
+              >
+                Home
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/about">About</Link>
+              <Link
+                className="nav-link"
+                to="/about"
+                style={{
+                  color: "#00ffe0",
+                  fontSize: "1.25rem",
+                  fontWeight: "600",
+                  textShadow: "0 0 5px #00ffe0",
+                }}
+              >
+                About
+              </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">Contact Us</Link>
+              <Link
+                className="nav-link"
+                to="/contact"
+                style={{
+                  color: "#00ffe0",
+                  fontSize: "1.25rem",
+                  fontWeight: "600",
+                  textShadow: "0 0 5px #00ffe0",
+                }}
+              >
+                Contact Us
+              </Link>
             </li>
           </ul>
         </div>
