355abd0f82fe4796ec629f1ba8da410ec5f7dd3b
anjum44 <anjumafrin303@gmail.com>
2025-07-13 00:13:50 +0600

    about us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import CharacterDetail from "./pages/CharacterDetail";
+import About from "./pages/About";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <div style={{ padding: "10px", background: "#f0f0f0" }}>
+        <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
+        <Link to="/about">About</Link>
+      </div>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/character/:id" element={<CharacterDetail />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,20 @@ export default function About() {
     <div className="container my-4">
       <h2>About Rick & Morty Explorer</h2>
       <p>
-        This is a simple React app that lets you browse characters from the Rick
-        & Morty universe. Built with students of CSE-3100.
+        This is a simple React app that lets you browse characters from the Rick & Morty universe.
+        Built with students of CSE-3100.
       </p>
+
+      <h4>Developer</h4>
+      <p>
+        Developed by [Anjum Hossain], a student exploring React and APIs.
+      </p>
+
+      <h4>Favorite Rick & Morty Quote</h4>
+      <blockquote className="blockquote">
+        <p>"Nobody exits on purpose,nobody belongs anywhere,everybody's gonna die.come watch TV!"</p>
+        <footer className="blockquote-footer">Morty</footer>
+      </blockquote>
     </div>
   );
 }
