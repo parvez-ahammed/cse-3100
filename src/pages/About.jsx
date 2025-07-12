8e53f2f90b7a2e7253e25a5e45d64017c78f2998
Mohsina Rahman Mirza <142750535+Mohsina19@users.noreply.github.com>
2025-07-12 15:46:41 +0600

    Removed Emojis

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,14 +9,14 @@ export default function About() {
 
       <hr />
 
-      <h4 className="mt-4">👩‍💻 Developer Info</h4>
+      <h4 className="mt-4"> Developer Info</h4>
       <p>
         Developed by <strong>Mohsina Rahman Mirza</strong>, a student of the Department of Computer Science and Technology, currently in 3rd Year, 1st Semester. This project is part of a learning journey with React.js and API integration.
       </p>
 
       <hr />
 
-      <h4 className="mt-4">🧠 Favorite Rick & Morty Quote</h4>
+      <h4 className="mt-4">Favorite Rick & Morty Quote</h4>
       <blockquote className="blockquote">
         <p className="mb-0">
           "Sometimes science is more art than science, Morty. A lot of people don’t get that."
