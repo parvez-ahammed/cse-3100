87dc27045853a1072ed251ec352525b0a8656791
mahdisByte <mahdikhan.chowdhury@gmail.com>
2025-07-11 23:35:30 -0700

    nabar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-import CharacterDetail from "./pages/CharacterDetail"; // Make sure this path is correct
+import CharacterDetail from "./pages/CharacterDetail";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
